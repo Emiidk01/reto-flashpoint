c125587398d04267eda0ed54e855485aa184e4a4
Emiliano <a01710711@tec.mx>
2024-08-30 15:05:13 -0600

    Add template for sending data via json and some prefabs

--- tc2008B-flashpoint/Assets/Scripts/Data/Network/WebClient.cs
+++ tc2008B-flashpoint/Assets/Scripts/Data/Network/WebClient.cs
@@ -3,19 +3,13 @@
 // Sergio Ruiz-Loza, Ph.D. March 2021
 
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
-using Newtonsoft.Json.Linq;
+
 
 public class WebClient : MonoBehaviour
 {
-    public GameObject wallPrefab;
-    public GameObject firePrefab;
-    public GameObject poiPrefab;
-    public GameObject doorPrefab;
-    public GameObject entryPrefab;
+    public GameObject sphere;  // La esfera que quieres mover
 
     // IEnumerator - yield return
     IEnumerator SendData(string data)
@@ -28,45 +22,30 @@ public class WebClient : MonoBehaviour
             byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(data);
             www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
             www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-            //www.SetRequestHeader("Content-Type", "text/html");
             www.SetRequestHeader("Content-Type", "application/json");
 
-            yield return www.SendWebRequest();          // Envia la solicitud al servidor de python
+            yield return www.SendWebRequest();          // Envía los datos al servidor
             if (www.isNetworkError || www.isHttpError)
             {
                 Debug.Log(www.error);
             }
             else
             {
-                // Procesa la respuesta recibida por el servidor
-                Debug.Log(www.downloadHandler.text);    // ARespuesta de python
-                JObject mapData = JObject.Parse(www.downloadHandler.text);
-                RenderMap(mapData);
-                //Debug.Log("Form upload complete!");
+                // Recibe la respuesta del servidor y mueve la esfera
+                Debug.Log(www.downloadHandler.text);
+                Vector3 newPosition = JsonUtility.FromJson<Vector3>(www.downloadHandler.text.Replace('\'', '\"'));
+                sphere.transform.position = newPosition;
+                Debug.Log("Esfera movida a: " + newPosition);
             }
         }
-    }
 
-    // Método para renderizar el mapa basado en los datos recibidos
-    void RenderMap(JObject mapData)
-    {
-        // Aquí deberías implementar la lógica para instanciar los objetos en el mapa
-        // Utiliza los datos de 'mapData' para posicionar paredes, fuegos, POIs, puertas, etc.
-        Debug.Log("Map data received and parsed.");
     }
-
 
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(SendData(call));
+        Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);  // Posición inicial de prueba
+        string json = JsonUtility.ToJson(fakePos);
         StartCoroutine(SendData(json));
-        // transform.localPosition
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Puedes agregar aquí lógica que necesite actualizaciones por frame, si es necesario
     }
 }
